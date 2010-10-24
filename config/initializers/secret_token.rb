d7351212b1daf13b2315706e332dc14287055364
praWin <pravinhmhatre@gmail.com>
2010-10-24 23:12:44 +0530

    Addin application code with layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SalCalNew::Application.config.secret_token = 'f020185dc632cde57ce99d09b635b5c2fde0a69e63d2955e1410734ff8a915572ec06f1f7482b9666357cadb6510c63a6e2df8d6e0960eb334e756dc259e4844'
