d8df171ce73b624dd8b176b6aebdf4867d473716
praWin <pravinhmhatre@gmail.com>
2010-11-08 22:58:16 +0530

    -- modified expense form and listing

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -7,10 +7,12 @@ class ExpensesController < ApplicationController
   end
 
   def create
-    @expense = Expense.new(params[:expense])
-    puts params.inspect
+    @expense = Expense.new(params[:expense].merge!({:user_id => current_user.id}))
     if @expense.save
-      ajax_alert('Expense saved successfully')
+      render :update do |page|
+        page << "$('form#new_expense')[0].reset()"
+      end
+      #ajax_alert('Expense saved successfully')
     else
       ajax_alert('Problem saving expense !!!')
     end
